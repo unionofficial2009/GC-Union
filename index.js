788c356348df378830d5a7775b20b86549af06fb
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-11-18 08:05:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,46 +23,7 @@ let args =  messageArray.slice(1);
     
  if(cmd === `${prefix}present`){
    
-    let gcmemberRole1 = message.guild.roles.find("name", "GC - Member");
-    //let gcmemberRole2 = message.guild.roles.find("name", "GC - Member 2.0");
-   
-   if(message.member.roles.has(gcmemberRole1.id)) {
-   if(cooldown.has(message.author.id)){
-    message.delete();
-  return message.reply("You have to wait 1 day.")
-  }
-  cooldown.add(message.author.id);
-     
-  let c_user = message.author   
-  let bicon = c_user.displayAvatarURL;   
-  let bicon2 = bot.user.displayAvatarURL;   
-  //let userroles = message.member.roles.filter(g => g.name != "@everyone").map(u=> `<@&${u.id}>`).join(", ");   
-     
-  let attendanceEmbed = new Discord.RichEmbed()
-  .setDescription(`${message.author}`)
-  .addField("Username", `${message.author.username}`)
-  .addField("Tag", `${message.author.tag}`)
-  .addField("ID", `${message.author.id}`)
-  //.addField("Roles", `${userroles}`)
-  .setColor("#15f153")
-  .setThumbnail(bicon)
-  .addField("Attendance", "Present")
-  .setTimestamp()
-  .setFooter("UNION GC Attendance",bicon2);
-  
-  let attendancechannel = message.guild.channels.find(`name`, "gc-attendance");
-  if (!attendancechannel) return message.channel.send("Couldn't find attendance channel.");
-  
-  
-  message.delete().catch(O_o=>{});
-  attendancechannel.send(attendanceEmbed);
-      } else {
-     message.reply("You don't have the permission to use this command.");
-   }
-   setTimeout(() => {
-      cooldown.delete(message.author.id)
-      }, cdseconds * 1000)
-
+    
     
   }
   
